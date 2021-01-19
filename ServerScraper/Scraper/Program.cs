858da2c9a5911b3aee2e5d7c40ad0a5353335d51
Whanos <dejavuplayzstuff@gmail.com>
2021-01-19 23:23:16 +0000

    Added error catching for guild not in cache
    discord pls

--- ServerScraper/Scraper/Program.cs
+++ ServerScraper/Scraper/Program.cs
@@ -43,15 +43,22 @@ namespace ServerScraper
             using (var write = new StreamWriter("UserIDs.txt", false))
             {
                 write.WriteLine("---[USERIDs]---");
-                var guild = client.GetCachedGuild(Serverid);
-                var mcount = guild.MemberCount;
-                if (mcount >= 1000) Console.WriteLine("[WARN] Server has 1000+ members, expect quirky behaviour!");
-                using (StreamWriter writer = new StreamWriter("UserList.txt", true))
+                try
                 {
-                    foreach (GuildMember member in client.GetGuildChannelMembers(guild, Channelid))
+                    var guild = client.GetCachedGuild(Serverid);
+                    var mcount = guild.MemberCount;
+                    if (mcount >= 1000) Console.WriteLine("[WARN] Server has 1000+ members, expect quirky behaviour!");
+                    using (StreamWriter writer = new StreamWriter("UserList.txt", true))
                     {
-                        writer.WriteLine(member.User.Id);
+                        foreach (GuildMember member in client.GetGuildChannelMembers(guild, Channelid))
+                        {
+                            writer.WriteLine(member.User.Id);
+                        }
                     }
+                }
+                catch (DiscordHttpException e)
+                {
+                    Console.WriteLine("This guild is not cached! Sorry, this is an API limitation.\n" + e);
                 }
             }
             Console.WriteLine("Done!");
@@ -59,8 +66,6 @@ namespace ServerScraper
             Thread.Sleep(2000);
             Environment.Exit(0);
         }
-
-
         private class Config
         {
             public string CToken { get; set; }
